f52524631c710566de6d85018fed5e1440403fbd
sunny(sAM) <101540270+sunnykumarsam@users.noreply.github.com>
2022-05-25 22:44:57 +0530

    if_statement
    25/5/2022

---
+++ if_statement.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int number=0;
+    printf("Enter the number:-");
+    scanf("%d",&number);
+    if(number%2==0){
+        printf("the number is even",number);
+       if(number%2==)
+                   printf("the number is odd",number);
+        }
+    }
+    return 0;
+}
