85200ba24104ca68131180ea7753f475d5871c74
sunny(sAM) <101540270+sunnykumarsam@users.noreply.github.com>
2022-07-05 23:04:39 +0530

    if_else_loop.c
    5/7/2022

---
+++ if_else.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <conio.h>
+int main(){
+    int age;
+printf("Enter your age:_");
+scanf("%d",&age);
+if(age>=18){
+    printf("you are the eligible for vote:-");
+
+}else{
+    printf("you are not eligible for vote:-");
+}
+return 0;
+
+}
