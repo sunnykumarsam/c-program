fe76f00f132f957816e13ddae4636474f6213d81
sunny(sAM) <101540270+sunnykumarsam@users.noreply.github.com>
2022-07-05 22:33:37 +0530

    add_fun.c
    5/7/2022

---
+++ add_func.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int sum(int  a ,int  b){
+    // return a+b;
+}
+
+int main(){
+    int a ,b,c;
+    a=4;
+    b=5;
+    c=sum(a,b);
+    printf("sum of a and b:-%d",c);
+
+}
+ int sum(int x,int y);
+ return c;
